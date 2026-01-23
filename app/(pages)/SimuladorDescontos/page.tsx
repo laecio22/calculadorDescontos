31283d5dfbfb40f47beddf0b866847928dafa3a1
laecio22 <laecon22@hotmail.com>
2026-01-23 16:48:31 -0300

    feat:criação  do  componente  de  input  do  valor  e  página  principal simulador  de  descontos

---
+++ app/(pages)/SimuladorDescontos/page.tsx
@@ -0,0 +1,14 @@
+import EntradaValor from "@/app/_components/EntradaValor"
+
+const SimuladorDescontos = () => {
+  return (
+    <section>
+        <h2>Aplicativo</h2>
+        <form action="" >
+          <EntradaValor/>
+        </form>
+    </section>
+  )
+}
+
+export default SimuladorDescontos
---
+++ app/_components/EntradaValor/index.tsx
@@ -0,0 +1,12 @@
+const EntradaValor = () => {
+  return (
+    <input
+      type="number"
+      min={0}
+      className="border border-gray-300"
+      placeholder="Informe valor "
+    />
+  );
+};
+
+export default EntradaValor;
