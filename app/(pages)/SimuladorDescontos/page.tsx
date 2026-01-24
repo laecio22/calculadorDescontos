8411c58af33de35aca98a73cd436cccd28e2839d
laecio22 <laecon22@hotmail.com>
2026-01-24 19:20:27 -0300

    feat:Criação  do componente  para  selecionar  o tipo de  usuário

--- app/(pages)/SimuladorDescontos/page.tsx
+++ app/(pages)/SimuladorDescontos/page.tsx
@@ -1,9 +1,14 @@
 'use client'
 import EntradaValor from "@/app/_components/EntradaValor";
+import SeletorCliente from "@/app/_components/SeletorCliente";
 import { useState } from "react";
 
 const SimuladorDescontos = () => {
-  const [valor, setValor] =  useState('')
+  const [valor, setValor] =  useState("")
+  const [mensagem, setMensagem] =  useState("")
+  const [tipoUsuario, setTipoUsuario] =  useState(null)
+  const [valorFinal, setValorFinal] = useState("")
+
   return (
     <section className=" h-100 
       w-3xl
@@ -15,6 +20,7 @@ const SimuladorDescontos = () => {
       " >
         <h2 className="font-bold text-3xl ">Simulador de Descontos</h2>
         <EntradaValor valor={valor} aoDigitar={setValor} />
+        <SeletorCliente/>
         {valor}
       </form>
     </section>
---
+++ app/_components/SeletorCliente/index.tsx
@@ -0,0 +1,14 @@
+
+const SeletorCliente = () => {
+  return (
+    <select>
+        <option value="">Selecione o tipo de  cliente</option>
+        <option value="">VIP</option>
+        <option value="">Funcionário</option>
+        <option value="">Parceiro</option>
+          <option value="">Comum</option>
+    </select>
+  )
+}
+
+export default SeletorCliente
