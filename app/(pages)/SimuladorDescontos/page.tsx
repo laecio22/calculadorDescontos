f0c5602f6997647010593679cdee06e0fab31d4b
laecio22 <laecon22@hotmail.com>
2026-01-25 14:10:14 -0300

    style:estilização  do componente  de  select  de  usuários

--- app/(pages)/SimuladorDescontos/page.tsx
+++ app/(pages)/SimuladorDescontos/page.tsx
@@ -1,27 +1,33 @@
-'use client'
+"use client";
 import EntradaValor from "@/app/_components/EntradaValor";
 import SeletorCliente from "@/app/_components/SeletorCliente";
 import { useState } from "react";
 
 const SimuladorDescontos = () => {
-  const [valor, setValor] =  useState("")
-  const [mensagem, setMensagem] =  useState("")
-  const [tipoUsuario, setTipoUsuario] =  useState(null)
-  const [valorFinal, setValorFinal] = useState("")
+  const [valor, setValor] = useState("");
+  const [mensagem, setMensagem] = useState("");
+  const [tipoUsuario, setTipoUsuario] = useState("");
+  const [valorFinal, setValorFinal] = useState(null);
 
   return (
-    <section className=" h-100 
+    <section
+      className=" h-100 
       w-3xl
      bg-cyan-50
       rounded-lg
-      ">
-      <form action="" className="flex flex-col gap-4
+      "
+    >
+      <form
+        action=""
+        className="flex flex-col gap-8
        items-center p-6
-      " >
+      "
+      >
         <h2 className="font-bold text-3xl ">Simulador de Descontos</h2>
         <EntradaValor valor={valor} aoDigitar={setValor} />
-        <SeletorCliente/>
+        <SeletorCliente typeUser={tipoUsuario} aoSelecionar={setTipoUsuario} />
         {valor}
+        {tipoUsuario}
       </form>
     </section>
   );
--- app/_components/SeletorCliente/index.tsx
+++ app/_components/SeletorCliente/index.tsx
@@ -1,12 +1,22 @@
-
-const SeletorCliente = () => {
+type UserProps = {
+  typeUser : string ;
+  aoSelecionar: (e:string) => void
+}
+const SeletorCliente = ({typeUser, aoSelecionar}:UserProps) => {
   return (
-    <select>
+    <select className="border
+     border-cyan-800
+      w-9/12
+       h-10 px-3 
+       rounded-lg"
+       value={typeUser}
+       onChange={(e) => aoSelecionar(e.target.value)}
+       >
         <option value="">Selecione o tipo de  cliente</option>
-        <option value="">VIP</option>
-        <option value="">Funcionário</option>
-        <option value="">Parceiro</option>
-          <option value="">Comum</option>
+        <option value="vip">VIP</option>
+        <option value="funcionario">Funcionário</option>
+        <option value="parceiro">Parceiro</option>
+          <option value="comum">Comum</option>
     </select>
   )
 }
