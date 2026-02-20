70026e1e68ac0b27be510c14fbc8c3591081c079
laecio22 <laecon22@hotmail.com>
2026-02-20 14:59:54 -0300

    refactor(Home): organização estilização  das  classes  tailwind em variáveis css no  arquivo Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import SimuladorDescontos from "./(pages)/SimuladorDescontos/page";
 
+const styles = {
+  tamanho: "min-h-screen",
+  cor: "bg-gray-800",
+  alinhamento: "flex flex-col items-center justify-center",
+};
 const Home = () => {
   return (
-    <main className="bg-gray-800  min-h-screen flex flex-col items-center justify-center ">   
+    <main className={`${styles.tamanho} ${styles.alinhamento} ${styles.cor}`}>
       <SimuladorDescontos />
     </main>
   );
